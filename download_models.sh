6a1c4a12be891fefe36177e10ee09bc233577b04
gsilva2016 <gabriel.l.silva@intel.com>
2023-03-27 11:19:32 -0700

    add model download for mask

--- download_models.sh
+++ download_models.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-if [ -d models ]; then
-	echo "Models dir already exists. Nothing to do."
-	exit 0
-fi
 
-mkdir -p models; cd models
+cd models/instance_segmentation_omz_1040/FP16-INT8/1
 wget https://storage.openvinotoolkit.org/repositories/open_model_zoo/2022.3/models_bin/1/instance-segmentation-security-1040/FP16-INT8/instance-segmentation-security-1040.bin
 wget https://storage.openvinotoolkit.org/repositories/open_model_zoo/2022.3/models_bin/1/instance-segmentation-security-1040/FP16-INT8/instance-segmentation-security-1040.xml
-cd ..
+cd ../../..
 
